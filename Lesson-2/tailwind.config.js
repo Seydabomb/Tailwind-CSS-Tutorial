39ca98155e7cb63573f89d5029104e9300b8933c
Seyona C <165888173+Seydabomb@users.noreply.github.com>
2024-08-29 11:57:18 -0600

    Finished with the next video
    Finished with the next video

--- Lesson-2/tailwind.config.js
+++ Lesson-2/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ["./build/*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				// At least 3 times as wide as it is 2 times tall
+				widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+				// At least twice as tall as it is wide
+				tallscreen: { raw: "(min-aspect-ratio: 1/2)" },
+			},
+		},
 	},
 	plugins: [],
 };
